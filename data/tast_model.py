a5f8568dd0dea187d7968942ccb1576bba5839c7
dmitrii1011sg <dmitrii1011sg@gmail.com>
2023-02-09 23:32:58 +0300

    added get, post request

--- data/tast_model.py
+++ data/tast_model.py
@@ -10,3 +10,16 @@ class Task(SqlAlchemyBase):
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     title = sqlalchemy.Column(sqlalchemy.String(150))
     content = sqlalchemy.Column(sqlalchemy.String)
+
+    def shortest_information(self):
+        return {
+            'title': self.title,
+            'created_at': self.created_date
+        }
+
+    def full_information(self):
+        return {
+            'title': self.title,
+            'content': self.content,
+            'created_at': self.created_date,
+        }
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-
-from flask import Flask, jsonify
+from data import db_session
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from flask_restful import Resource, Api
+
+from data.tast_model import Task
 
 load_dotenv()
 
@@ -10,14 +11,23 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Tasks(Resource):
+class TasksListAPI(Resource):
     def get(self):
-        return jsonify({'hello': 'get'})
+        db_sess = db_session.create_session()
+        tasks_list = list(map(lambda x: x.shortest_information(), db_sess.query(Task).all()))
+        total = len(tasks_list)
+        return jsonify({'tasks': tasks_list, 'total': total})
 
     def post(self):
-        return jsonify({'hello': 'post'})
+        json_data = request.get_json(force=True)
+        new_task = Task(title=json_data['title'], content=json_data['content'])
 
-class Task(Resource):
+        db_sess = db_session.create_session()
+        db_sess.add(new_task)
+        db_sess.commit()
+        return jsonify(new_task.full_information())
+
+class TaskAPI(Resource):
     def get(self, id):
         return jsonify({'Task': 'get'})
 
@@ -28,8 +38,9 @@ class Task(Resource):
         return jsonify({'Task': 'delete'})
 
 
-api.add_resource(Tasks, '/api/task/')
-api.add_resource(Task, '/api/task/<int:id>')
+api.add_resource(TasksListAPI, '/api/task/')
+api.add_resource(TaskAPI, '/api/task/<int:id>')
 
 if __name__ == '__main__':
+    db_session.global_init("database/database.db")
     app.run(debug=True)
