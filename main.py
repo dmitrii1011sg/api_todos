e7bb5030de638160c22e128c0391d7b322155af7
dmitrii1011sg <dmitrii1011sg@gmail.com>
2023-02-09 17:03:26 +0300

    flask restful

--- main.py
+++ main.py
@@ -2,17 +2,34 @@ import os
 
 from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_restful import Resource, Api
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('TOKEN')
+api = Api(app)
 
 
-@app.route('/api/task', methods=['GET'])
-def index():
-    return jsonify({'tasks': 123})
+class Tasks(Resource):
+    def get(self):
+        return jsonify({'hello': 'get'})
 
+    def post(self):
+        return jsonify({'hello': 'post'})
+
+class Task(Resource):
+    def get(self, id):
+        return jsonify({'Task': 'get'})
+
+    def put(self, id):
+        return jsonify({'Task': 'put'})
+
+    def delete(self, id):
+        return jsonify({'Task': 'delete'})
+
+
+api.add_resource(Tasks, '/api/task/')
+api.add_resource(Task, '/api/task/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
