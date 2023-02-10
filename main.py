aac389340c03330f7a426361722ea39bc50b9e65
dmitrii1011sg <dmitrii1011sg@gmail.com>
2023-02-10 22:27:27 +0300

    refactor named values

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ app = Flask(__name__)
 api = Api(app)
 
 
-def abort_if_task_doesnt_exist(id):
+def abort_if_task_doesnt_exist(id_task):
     db_sess = db_session.create_session()
-    task = db_sess.query(Task).filter(Task.id == id).first()
+    task = db_sess.query(Task).filter(Task.id == id_task).first()
     if not task:
-        abort(404, error_code="404", message=f"Task {id} doesn't exist")
+        abort(404, error_code="404", message=f"Task {id_task} doesn't exist")
+
 
 class TasksListAPI(Resource):
     def get(self):
@@ -33,30 +34,30 @@ class TasksListAPI(Resource):
 
 
 class TaskAPI(Resource):
-    def get(self, id):
-        abort_if_task_doesnt_exist(id)
+    def get(self, id_task):
+        abort_if_task_doesnt_exist(id_task)
         db_sess = db_session.create_session()
-        task = db_sess.query(Task).filter(Task.id == id).first()
+        task = db_sess.query(Task).filter(Task.id == id_task).first()
         return jsonify(task.full_information())
 
-    def put(self, id):
-        abort_if_task_doesnt_exist(id)
+    def put(self, id_task):
+        abort_if_task_doesnt_exist(id_task)
         json_data = request.get_json(force=True)
         db_sess = db_session.create_session()
-        db_sess.query(Task).filter(Task.id == id).update(json_data)
+        db_sess.query(Task).filter(Task.id == id_task).update(json_data)
         db_sess.commit()
-        return jsonify(db_sess.query(Task).filter(Task.id == id).first().full_information())
+        return jsonify(db_sess.query(Task).filter(Task.id == id_task).first().full_information())
 
-    def delete(self, id):
-        abort_if_task_doesnt_exist(id)
+    def delete(self, id_task):
+        abort_if_task_doesnt_exist(id_task)
         db_sess = db_session.create_session()
-        db_sess.query(Task).filter(Task.id == id).delete()
+        db_sess.query(Task).filter(Task.id == id_task).delete()
         db_sess.commit()
         return '', 204
 
 
 api.add_resource(TasksListAPI, '/api/task/')
-api.add_resource(TaskAPI, '/api/task/<int:id>')
+api.add_resource(TaskAPI, '/api/task/<int:id_task>')
 
 if __name__ == '__main__':
     db_session.global_init("database/database.db")
